8a9927b70911d60bf657341cb6c1d3c460bd3e12
rogok <kamil.rogoda@pega.com>
2020-08-24 11:49:53 +0200

    Code clarification

--- mmock-runtime/src/androidMain/kotlin/com/pega/mmock/backend/unsafe/UnsafeInstance.kt
+++ mmock-runtime/src/androidMain/kotlin/com/pega/mmock/backend/unsafe/UnsafeInstance.kt
@@ -10,25 +10,44 @@ import net.sf.cglib.proxy.MethodProxy
 import org.objenesis.ObjenesisHelper
 
 actual inline fun <reified T> createUnsafe(): T {
-    return if (T::class.isAbstract) {
-        val enhancer = Enhancer()
-        val interceptor = MethodInterceptor { _: Any, _: Method, _: Array<Any>, _: MethodProxy -> }
-        enhancer.setSuperclass(T::class.java)
-        enhancer.setCallbackType(interceptor.javaClass)
-        val proxy = enhancer.createClass()
-        ObjenesisHelper.newInstance(proxy) as T
-    } else if (T::class.isSealed) {
-        val subclasses = T::class.sealedSubclasses
-        if (subclasses.isEmpty())
-            throw SealedClassNoSubclassException("${T::class.simpleName} does not have any subclass defined. It's not efficient to declare sealed class without subclasses.")
-        UnsafeAndroid().allocateInstance(subclasses[0].java)
-    } else if (T::class.java.isInterface) {
-        Proxy.newProxyInstance(
-            T::class.java.classLoader,
-            arrayOf(T::class.java),
-            ThrowingInvocationHandler()
-        ) as T
-    } else {
-        UnsafeAndroid().allocateInstance(T::class.java)
+    return when {
+        T::class.isSealed -> createUnsafeSealed()
+        T::class.isAbstract -> createUnsafeAbstract()
+        T::class.java.isInterface -> createUnsafeInterface()
+        else -> UnsafeAndroid().allocateInstance(T::class.java)
     }
+}
+
+/*
+ * Abstract class instance created using cglib and objenesis. Instance is created without using constructor.
+ */
+inline fun <reified T> createUnsafeAbstract(): T {
+    val enhancer = Enhancer()
+    val interceptor = MethodInterceptor { _: Any, _: Method, _: Array<Any>, _: MethodProxy -> }
+    enhancer.setSuperclass(T::class.java)
+    enhancer.setCallbackType(interceptor.javaClass)
+    val proxy = enhancer.createClass()
+    return ObjenesisHelper.newInstance(proxy) as T
+}
+
+/*
+ * Cglib and objenesis cannot be used to create instance of sealed class.
+ * Instance of subclass created instead (throw exception if there is no subclass - known issue).
+ */
+inline fun <reified T> createUnsafeSealed(): T {
+    val subclasses = T::class.sealedSubclasses
+    if (subclasses.isEmpty())
+        throw SealedClassNoSubclassException("${T::class.simpleName} does not have any subclass defined. It's not efficient to declare sealed class without subclasses.")
+    return UnsafeAndroid().allocateInstance(subclasses[0].java)
+}
+
+/*
+ * Using interface proxy to create an instance of the interface.
+ */
+inline fun <reified T> createUnsafeInterface(): T {
+    return Proxy.newProxyInstance(
+        T::class.java.classLoader,
+        arrayOf(T::class.java),
+        ThrowingInvocationHandler()
+    ) as T
 }
