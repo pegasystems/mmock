2508df4b816de9d17ae8de02dcd5cb35b76e9ae4
Kamil Rogoda <rogoda.kamil@gmail.com>
2020-09-17 13:41:02 +0200

    ISSUE-2: changed .travis.yml file (#16)
    * ISSUE-2: changed .travis.yml file
    
    * ISSUE-2: add JDK for Windows
    
    * ISSUE-2: remove language specification
    
    * ISSUE-2: .travis.yml update
    
    * ISSUE-2: .travis.yml update
    
    * ISSUE-2: remove Windows (temp), specify JDK
    
    * ISSUE-2: update .travis.yml
    
    * ISSUE-2: remove specific jdk version
    
    * ISSUE-2: macos/ios integration test
    
    * ISSUE-2: edit demo-project build.gradle
    
    * ISSUE-2: add integration tests
    
    * ISSUE-2: update .travis.yml
    
    * ISSUE-2: update .travis.yml
    
    * ISSUE-2: update .travis.yml
    
    * ISSUE-2: merge unit and integration tests
    
    * ISSUE-2: split apple targets
    
    * ISSUE-2: update .travis.yml

--- demo-project/sub-module/build.gradle.kts
+++ demo-project/sub-module/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("multiplatform")
     id("org.jlleitschuh.gradle.ktlint")
@@ -64,4 +66,8 @@ kotlin {
             }
         }
     }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
