d82f43228572c5fa97af4fa975362a29572a461f
kamilr12 <rogoda.kamil@gmail.com>
2020-09-18 10:57:27 +0200

    ISSUE-11: add missing Apache 2.0 copyright info

--- mmock-runtime/src/commonMain/kotlin/com/pega/mmock/backend/exception/SealedClassNoSubclassException.kt
+++ mmock-runtime/src/commonMain/kotlin/com/pega/mmock/backend/exception/SealedClassNoSubclassException.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2020 Pegasystems Inc. All rights reserved.
+ * Use of this source code is governed by a Apache 2.0 license that can be found in the LICENSE file.
+ */
+
 package com.pega.mmock.backend.exception
 
 class SealedClassNoSubclassException(override val message: String) : Exception()
